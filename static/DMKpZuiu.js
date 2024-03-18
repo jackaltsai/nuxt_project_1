df7b20b71608e8c8a58cfba17ece8f1b717d1ae4
Zyko <hata.s520@gmail.com>
2024-03-18 19:03:23 +0800

    Updates

--- static/DMKpZuiu.js
+++
@@ -1,1 +0,0 @@
-import{d as o,L as t,t as s,v as n}from"./zeEsUmnO.js";const l=o({__name:"[id]",setup(a){const e=t();return console.log(e.params.id),(r,c)=>(s(),n("h1",null,"posts"))}});export{l as default};
